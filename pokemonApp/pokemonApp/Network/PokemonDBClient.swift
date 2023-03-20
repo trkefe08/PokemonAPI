e0cf802cc1349664b9532921ffbdd697da6f19e4
Tarık <trkefe08@gmail.com>
2023-03-20 23:56:20 +0300

    The project was created and the makeRequest function was written for network operations.

---
+++ pokemonApp/pokemonApp/Network/PokemonDBClient.swift
@@ -0,0 +1,33 @@
+//
+//  PokemonDBClient.swift
+//  pokemonApp
+//
+//  Created by Tarik Efe on 20.03.2023.
+//
+
+import Foundation
+
+final class PokemonDBCLient {
+    
+    static let shared = PokemonDBCLient()
+    private init() {}
+    
+    //MARK: - Request
+    func makeRequest<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } catch let error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+}
---
+++ pokemonApp/pokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonApp
+//
+//  Created by Tarik Efe on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
